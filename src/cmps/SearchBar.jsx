c903c3987f05cd6a310bdb23f7b7606fd7a4eb0f
Daniel Schwartz <danielonai14@gmail.com>
2021-09-23 20:40:58 +0300

    ddd

--- src/cmps/SearchBar.jsx
+++ src/cmps/SearchBar.jsx
@@ -1,46 +1,20 @@
 
-import React from 'react'
+import {Component} from 'react'
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { Button, TextField } from '@material-ui/core';
+export class SerachBar extends Component {
 
-export const SearchBar = () => {
-  const initialValues = { location: '', checkIn: '', checkOut: '', guests: '' }
+  state={}
 
-  const onSubmit = () => {
 
-  }
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="text" name="Locatidcdcdon" label="location" />
-          <ErrorMessage name="text" component="div" />
-          <Field type="date" name="Check in" label="check-in" />
-          <ErrorMessage name="check-in" component="div" />
-          <Field type="date" name="Check out" label="check-out" />
-          <ErrorMessage name="check-out" component="div" />
-          <Field type="number" name="Guests" label="guests" />
-          <ErrorMessage name="guests" component="div" />
-          <Button
-            variant={'contained'}
-            color={'primary'}
-            type="submit"
-            disabled={isSubmitting}>
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  )
+
+
+
+
+
 
 
 }
-
 
 
 

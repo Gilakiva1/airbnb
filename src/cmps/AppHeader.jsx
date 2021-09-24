840ca191a1d5ab4144f73a9536b8e81e942a889e
Guy snir <guy.snir97@gmail.com>
2021-09-24 16:38:33 +0300

    logo svg

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,25 +1,27 @@
 // import React from 'react'
 
 import { NavLink } from "react-router-dom";
-import ReactLogo from '../assets/img/logo.svg';
 import HeroLogo from '../assets/img/hero.png';
+import { ReactComponent as AirbnbLogo } from '../assets/img/airbnb_logo.svg';
 
 
 
 export function AppHeader() {
     return (
         <>
-            <img className="hero-logo full" src={HeroLogo} />
+            <section className="hero-logo ">
+                <img src={HeroLogo} alt="" />
+            </section>
             <header className="header-container main-container">
-                <div className="header-func">
-                    <div className="logo-container">
-                        <img className="logo-img" src={ReactLogo} alt="" />
+                <div className="header-func flex">
+                    <div className="logo-container flex align-center">
+                            <AirbnbLogo className="logo-img" fill='white' />
                         <h3>Airbnb</h3>
                     </div>
                     <input className='header-search' type="search" />
-                    <div className="nav-header">
+                    <div className="nav-header flex align-center">
 
-                        <NavLink to={`/`} >Become a host</NavLink>
+                        <NavLink className="link-host" to={`/`} >switch to hosting</NavLink>
                         <button>=</button>
                     </div>
                 </div>

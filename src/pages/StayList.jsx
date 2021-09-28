955ca1846454e361ad329022c04672676c72d921
Gilakiva1 <gilakiva3@gmail.com>
2021-09-28 12:44:43 +0300

    fix bug

--- src/pages/StayList.jsx
+++ src/pages/StayList.jsx
@@ -4,17 +4,28 @@ import { StayPreview } from '../cmps/StayPreview.jsx'
 import { withRouter } from 'react-router'
 import { StayFilter } from '../cmps/StayFilter.jsx'
 import { loadStays } from '../store/stay.action.js'
+const queryString = require('query-string');
 
-import PriceFilter from '../cmps/PriceFilter.jsx'
 class _StayList extends React.Component {
 
     state = {}
 
     componentDidMount() {
+        let params1 = queryString.parse(this.props.location.search)
+        console.log(params1);
+        const params = new URLSearchParams(this.props.location)
+        console.log('params :',params);
+        const city = new URLSearchParams(this.props.location.search).get("city")
+        console.log('city :',city);
+        // const queryString = location.search;
+        // const urlParams = new URLSearchParams(queryString);
+        // const page_type = urlParams.get('city')
+        // console.log('city', page_type);
         this.props.loadStays()
     }
-    
+
     render() {
+
         const { stays } = this.props
         if (!stays) return <div>loading...</div>
 

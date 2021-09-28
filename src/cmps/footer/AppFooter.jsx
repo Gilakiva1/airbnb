b17971c659072c181929fea8a2f182d405503a2e
Guy snir <guy.snir97@gmail.com>
2021-09-28 21:23:13 +0300

    header4.1

--- src/cmps/footer/AppFooter.jsx
+++ src/cmps/footer/AppFooter.jsx
@@ -3,7 +3,18 @@ export class AppFooter extends React.Component {
     render() {
         return (
             <footer className="main-footer main-container">
-                <div>footer!</div>
+
+                <div className="links-container">
+                    <div className="top rated">
+                        <h3></h3>
+                    </div>
+                    <div className="extra citis"> </div>
+                    <div className="nearby"> </div>
+                    <div className="about"> </div>
+                    <h3></h3>
+                </div>
+
+
             </footer>
         )
     }

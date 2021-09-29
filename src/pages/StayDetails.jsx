9e288fa1d2c07310714eecdbf225e64e2892c223
Daniel Schwartz <danielonai14@gmail.com>
2021-09-29 19:02:52 +0300

    dates fixed

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -77,21 +77,7 @@ export class _StayDetails extends Component {
                         </div>
 
                     </div>
-                    <div className="seperation-line"></div>
-                    <div className="tag-container flex column">
-                        <div>
-                            {stay.tags.map((tag, idx) => (
-                                <Tags key={idx} tag={tag} type={stay.type} />
-                            ))}
-                        </div>
-                        <OrderModal />
-                    </div>
-                    <div className="seperation-line"></div>
-                    <div className="description">{stay.description}</div>
-                    <div className="seperation-line"></div>
-                    <div className="amenities">
-                        <h2>What this place offers</h2>
-                    </div>
+                
                     <OrderModal />
                 </div>
             </section>

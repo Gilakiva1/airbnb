4aaaf60adca2cdb758b0dc927977ef42b6625878
Guy snir <guy.snir97@gmail.com>
2021-10-06 18:52:03 +0300

    commit

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -36,9 +36,9 @@ async function ajax(endpoint, method = 'GET', data = null) {
     } catch (err) {
         console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: ${data}`)
         console.dir(err)
-        if (err.response && err.response.status === 401) {
-            window.location.assign('/')
-        }
+        // if (err.response && err.response.status === 401) {
+        //     window.location.assign('/')
+        // }
         throw err
     }
 }
